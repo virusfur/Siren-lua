a6577dd9dc6455b6d00e349b36cf7008632b70b3
virusfur <81878627+virusfur@users.noreply.github.com>
2025-05-02 14:06:50 +0300

    add coal, just sprite and collisions

--- main.lua
+++ main.lua
@@ -1,9 +1,19 @@
 wf = require 'Assets/Modules/windfield'
 anim8 = require 'Assets/Modules/anim8'
 
+local virtualWidth = 640
+local virtualHeight = 360
+local scale = 1
+local scaleX, scaleY = 1, 1
+local canvas
+
 function love.load()
-    love.window.setMode(640,360)
+    local winWidth, winHeight = 1280,720
+    love.window.setMode(winWidth,winHeight, {resizable=true})
     love.window.setTitle('Siren')
+    love.graphics.setDefaultFilter("nearest","nearest")
+
+    canvas = love.graphics.newCanvas(virtualWidth,virtualHeight)
 
     world = wf.newWorld(0,0,true)
 
@@ -17,10 +27,12 @@ function love.load()
     walls[2]:setType('static')
     walls[3]:setType('static')
     walls[4]:setType('static')
+    
+    Walls = love.graphics.newImage('Assets/Sprites/walls.png')
     ----------------
 
     --player init
-    player = world:newRectangleCollider(200,150, 32,44)
+    player = world:newBSGRectangleCollider(200,150, 32,44,8)
     player.speed = 80
     player.isLeft = false
     player:setFixedRotation(true)
@@ -30,7 +42,7 @@ function love.load()
     --set grid
     player.AnimGrid = anim8.newGrid(34,44, 272,92)
 
-    --animayions
+    --animations
     player.Animations = {}
     player.Animations.IdleClothes = anim8.newAnimation(player.AnimGrid('1-2', 1),0.5)
     player.Animations.WalkClothes = anim8.newAnimation(player.AnimGrid('3-4', 1),0.3)
@@ -40,16 +52,20 @@ function love.load()
 
     oven = {}
     oven.collider1 = world:newBSGRectangleCollider(180+20,20,134,37, 16)
-    oven.collider2 = world:newRectangleCollider(226+20,20+37,43,20)
+    oven.collider2 = world:newRectangleCollider(225+20,20+37,45,20)
     oven.collider1:setType('static')
     oven.collider2:setType('static')
     oven.texture = love.graphics.newImage('Assets/Sprites/oven.png')
 
-    
-    Walls = love.graphics.newImage('Assets/Sprites/walls.png')
+    coal = {}
+    coal.texture = love.graphics.newImage("Assets/Sprites/coal.png")
+    coal.collider = world:newRectangleCollider(430,250, 64,24)
+    coal.collider:setType("static")
 end
 
 function love.update(dt)
+    updateScale()
+
     world:update(dt)
 
     player.AnimSet = player.Animations.IdleClothes
@@ -60,14 +76,28 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear()
+
     love.graphics.draw(oven.texture, 180+20,20)
+
     if player.isLeft == false then
         player.AnimSet:draw(player.SpriteSheet, player:getX()-16, player:getY()-22)
     elseif player.isLeft == true then
         player.AnimSet:draw(player.SpriteSheet, player:getX()+16, player:getY()-22, nil, -1,1)
     end
+
     love.graphics.draw(Walls, 130,15)
-    world:draw()
+    love.graphics.draw(coal.texture, 430,224)
+
+    --world:draw()
+
+    love.graphics.setCanvas()
+
+    love.graphics.draw(canvas,
+    (love.graphics.getWidth() - virtualWidth * scale) * 0.5,
+    (love.graphics.getHeight() - virtualHeight * scale) * 0.5,
+    0, scale, scale)
 end
 
 function playerMovement()
@@ -96,4 +126,15 @@ function playerMovement()
 
     player:setLinearVelocity(vx,vy)
 
+end
+
+function updateScale()
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    scaleX = windowWidth / virtualWidth
+    scaleY = windowHeight / virtualHeight
+    scale = math.min(scaleX, scaleY)
+end
+
+function love.resize(w, h)
+    updateScale()
 end
